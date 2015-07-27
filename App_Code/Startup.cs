f4397462b54f1dce47a82782f4dacb7846ae2789
Shreyas <shreyas.gaonkar@yahoo.com>
2015-07-26 20:25:13 -0500

    Initial commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zenscorner.Startup))]
+namespace Zenscorner
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
